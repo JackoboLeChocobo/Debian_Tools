c5b8300c76b662fbf2877da707de72c763a19d5b
JackoboLeChocobo <imbert.jacquesolivier@gmail.com>
2024-11-10 10:01:40 +0100

    update

---
+++ Usagi-OS/linux-live/results/setup-legacy.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# Installation
+# Projet "Usagi OS"
+#
+
+for device in /sys/block/*
+do
+    if udevadm info --query=property --path=$device | grep -q ^ID_BUS=usb
+    then
+        dev=`udevadm info --query=property --path=$device | grep "^DEVNAME=" | sed "s/.*=//"`
+        name=`udevadm info --query=property --path=$device | grep "^ID_MODEL=" | sed "s/.*=//"`
+        donnees_liste=`echo $donnees_liste "$dev" "$name"`
+    fi
+done
+
+dev=`zenity \
+	--width=426 --height=240 \
+	--title="Usagi OS - Installation" \
+	--text="Choisissez le périphérique USB désiré" \
+	--list \
+	--column="Périphérique" \
+	--column="Nom" \
+	$donnees_liste 2> /dev/null`
+
+if echo "$dev" | grep '/'; then
+
+	echo " "
+	echo "Périphérique choisi : $dev"
+	echo " "
+	sleep 3
+
+	apt install progress grub-pc-bin
+
+	umount /media/install/mnt
+	umount /media/install/datas
+	umount /media/*/*
+
+	mkdir -pv /media
+	mkdir -pv /media/install
+	mkdir -pv /media/install/mnt
+	mkdir -pv /media/install/datas
+
+	wipefs -af ${dev}
+
+	(
+	echo o
+	echo n
+	echo p
+	echo 1
+	echo 
+	echo +600M
+	echo n
+	echo p
+	echo 2
+	echo 
+	echo 
+	echo t
+	echo 1
+	echo c
+	echo a
+	echo 1
+	echo w
+	) | sudo fdisk --wipe=always --wipe-partitions always $dev
+
+	systemctl daemon-reload
+
+	mkfs.fat -F32 ${dev}1
+	mkfs.ext4 ${dev}2
+		
+	mount -o umask=000 ${dev}1 /media/install/mnt
+	grub-install --removable --boot-directory=/media/install/mnt/boot --target=i386-pc $dev
+
+	uuid=`blkid -s UUID -o value ${dev}2`
+	sleep 10
+	
+	echo "menuentry 'Usagi OS'{" > /media/install/mnt/boot/grub/grub.cfg
+	echo "insmod part_msdos" >> /media/install/mnt/boot/grub/grub.cfg
+	echo "insmod ext2" >> /media/install/mnt/boot/grub/grub.cfg
+	echo "search --no-floppy --fs-uuid --set=root $uuid" >> /media/install/mnt/boot/grub/grub.cfg
+	echo "linux /Usagi-OS/boot/vmlinuz root=UUID=$uuid ro quiet splash perch" >> /media/install/mnt/boot/grub/grub.cfg
+	echo "initrd /Usagi-OS/boot/initrfs.img" >> /media/install/mnt/boot/grub/grub.cfg
+	echo "}" >> /media/install/mnt/boot/grub/grub.cfg
+
+	mount ${dev}2 /media/install/datas
+	echo "Copie des données en cours..."
+	sleep 3
+	cp -dR Usagi-OS /media/install/datas & progress -mp $!
+	chmod 777 -R /media/install/datas
+	
+	umount /media/install/mnt
+	umount /media/install/datas
+
+	rm -dr /media/install
+	
+	echo "Installation terminée !"
+fi
+
+
+
+
